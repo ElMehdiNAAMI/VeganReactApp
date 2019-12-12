2e14150f707fa1cb570990dda9d41e12f5fec3e2
El Mehdi NAAMI <naami.em@gmail.com>
2019-12-12 11:14:00 +0100

    store completed

--- src/Components/Exercices/index.js
+++ src/Components/Exercices/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
+
 export default function index() {
   return (
     <Grid container>
@@ -16,7 +17,7 @@ export default function index() {
 }
 
 const styles = {
-  Paper: {
+  paper: {
     padding: 20,
     marginTop: 10,
     marginBottom: 10
---
+++ src/store.js
@@ -0,0 +1,100 @@
+export const nutrients = ["carbs", "proteins", "fats"];
+
+export const sources = [
+  {
+    id: "Tofu",
+    title: "Tofu",
+    info:
+      "Soy products are among the richest sources of protein in a plant-based diet",
+    nutrients: "proteins"
+  },
+  {
+    id: "Lentils",
+    title: "Lentils",
+    info:
+      "Red or green lentils contain plenty of protein, fiber, and key nutrients, including iron and potassium.",
+    nutrients: "proteins"
+  },
+  {
+    id: "Chickpeas",
+    title: "Chickpeas",
+    info:
+      "Chickpeas, also known as garbanzo beans, are practically a staple ingredient in Middle Eastern cooking",
+    nutrients: "proteins"
+  },
+  {
+    id: "Edamame",
+    title: "Edamame",
+    info: "Edamame is a green soybean, very popular in East Asian cuisines",
+    nutrients: "proteins"
+  },
+  {
+    id: "Potatoes",
+    title: "Potatoes",
+    info: "The potato is a root vegetable native to the Americas.",
+    nutrients: "carbs"
+  },
+  {
+    id: "yams",
+    title: "yams",
+    info:
+      "Yams are carbohydrate rich, staple tuber vegetables of West African origin.",
+    nutrients: "carbs"
+  },
+  {
+    id: "Oatmeal",
+    title: "Oatmeal",
+    info:
+      "Oatmeal is a type of coarse flour made of hulled oat grains that have either been milled, steel-cut, or rolled.",
+    nutrients: "carbs"
+  },
+  {
+    id: "Rice",
+    title: "Rice",
+    info:
+      "As a cereal grain, it is the most widely consumed staple food for a large part of the world's human population,",
+    nutrients: "carbs"
+  },
+  {
+    id: "Pasta",
+    title: "Pasta",
+    info:
+      "Pasta is a dish originally from Italy consisting of dough made from durum wheat and water, extruded or stamped into various shapes and typically cooked in boiling water.",
+    nutrients: "carbs"
+  },
+  {
+    id: "Fruits",
+    title: "Fruits",
+    info:
+      "A fruit is generally a fleshy seed associated part of a particular plant; it is naturally and mostly edible and sweet in the raw state.",
+    nutrients: "carbs"
+  },
+  {
+    id: "Coconut",
+    title: "Coconut",
+    info:
+      "Coconut is a high source of saturated fat in the form of medium-chain triglyceride, which function differently to the long-chain fatty acids found in animal products",
+    nutrients: "fats"
+  },
+  {
+    id: "Avocado",
+    title: "Avocado",
+    info:
+      "Aside from being absolutely delicious, avocado has an impressive nutrient profile.",
+    nutrients: "fats"
+  },
+  {
+    id: "almond",
+    title: "almond",
+    info:
+      "The almond is a species of tree native to Iran and surrounding countries but widely cultivated elsewhere.",
+    nutrients: "fats"
+  },
+  {
+    id: "peanut",
+    title: "peanut",
+    info:
+      "The peanut, also known as the groundnut, goober, or monkey nut, and taxonomically classified as Arachis hypogaea.",
+    nutrients: "fats"
+  }
+];
