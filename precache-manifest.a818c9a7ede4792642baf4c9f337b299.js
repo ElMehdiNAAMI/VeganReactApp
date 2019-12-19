336bf1a1c16dd288475340a444c8034d82cc294c
El Mehdi NAAMI <naami.em@gmail.com>
2019-12-19 16:47:23 +0100

    Updates

---
+++ precache-manifest.a818c9a7ede4792642baf4c9f337b299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf31b3efedab164f38e681fbb69c038",
+    "url": "/VeganReactApp/index.html"
+  },
+  {
+    "revision": "564f876cd8e9315344cd",
+    "url": "/VeganReactApp/static/js/2.3fefd82a.chunk.js"
+  },
+  {
+    "revision": "1ffd7ad526f3e23bcc6f",
+    "url": "/VeganReactApp/static/js/main.d84c26dd.chunk.js"
+  },
+  {
+    "revision": "abcc67547a1868348117",
+    "url": "/VeganReactApp/static/js/runtime-main.ab57368c.js"
+  },
+  {
+    "revision": "f8cc6a256f329b1b92e18bfd0134fa8d",
+    "url": "/VeganReactApp/static/media/jam.f8cc6a25.png"
+  },
+  {
+    "revision": "560b74fdbbdc1591abf55246fde78e81",
+    "url": "/VeganReactApp/static/media/main.560b74fd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0199e112859a323f47f74fdc9091067.js"
+  "/VeganReactApp/precache-manifest.a818c9a7ede4792642baf4c9f337b299.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VeganReactApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
