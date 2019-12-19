b199e9aaf6f0fce74f258895b501390508ecf5c0
El Mehdi NAAMI <naami.em@gmail.com>
2019-12-19 16:30:27 +0100

    Updates

---
+++ precache-manifest.f0199e112859a323f47f74fdc9091067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88293a37a1f8630932cb0fd519c1506a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564f876cd8e9315344cd",
+    "url": "/static/js/2.3fefd82a.chunk.js"
+  },
+  {
+    "revision": "1ffd7ad526f3e23bcc6f",
+    "url": "/static/js/main.d84c26dd.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  },
+  {
+    "revision": "f8cc6a256f329b1b92e18bfd0134fa8d",
+    "url": "/static/media/jam.f8cc6a25.png"
+  },
+  {
+    "revision": "560b74fdbbdc1591abf55246fde78e81",
+    "url": "/static/media/main.560b74fd.jpg"
+  }
+]);
